7b8f4129526744f4ed6e231198e41c8002990f1f
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-07-14 21:16:26 -0300

    unidade 3 finalizada

---
+++ src/Pages/Cardapio/Buscador/index.tsx
@@ -0,0 +1,16 @@
+import styles from "./Buscador.module.scss";
+import { CgSearch } from "react-icons/cg";
+
+interface IBuscador {
+  busca: string,
+  setBusca: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Buscador({ busca, setBusca }: IBuscador) {
+  return (
+    <div className={styles.buscador}>
+      <input title="busca" type="text" value={busca} onChange={e => setBusca(e.target.value)} />
+      <CgSearch size={20} color="#4c4d5e" />
+    </div>
+  )
+}
