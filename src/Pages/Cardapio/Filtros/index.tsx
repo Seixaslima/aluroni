2a5ec8aa10ade4c9b4bde349c6ee7afa9ce58af8
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-07-19 11:34:28 -0300

    unidade 4 finalizada

---
+++ src/Pages/Cardapio/Filtros/index.tsx
@@ -0,0 +1,32 @@
+import styles from './Filtros.module.scss'
+import filtros from './filtros.json'
+import classNames from 'classnames'
+
+type IOpcao = typeof filtros[0]
+
+interface IFiltro {
+  filtro: number | null
+  setFiltro: React.Dispatch<React.SetStateAction<number | null>>
+}
+
+export default function Filtros({ filtro, setFiltro }: IFiltro) {
+
+  function selecionarFiltro(opcao: IOpcao) {
+    if (filtro === opcao.id) return setFiltro(null);
+    return setFiltro(opcao.id)
+
+  }
+
+  return (
+    <div className={styles.filtros}>
+      {filtros.map(opcao => (
+        <button key={opcao.id} onClick={() => selecionarFiltro(opcao)} className={classNames({
+          [styles.filtros__filtro]: true,
+          [styles['filtros__filtro--ativo']]: opcao.id === filtro
+        })}>
+          {opcao.label}
+        </button>
+      ))}
+    </div>
+  )
+}
---
+++ src/Pages/Cardapio/Ordenador/index.tsx
@@ -0,0 +1,39 @@
+import styles from './Ordenador.module.scss'
+import opcoes from './opcoes.json'
+import { useState } from 'react'
+import classNames from 'classnames';
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md';
+
+interface IOrdenador {
+  ordenador: string,
+  setOrdenador: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Ordenador({ ordenador, setOrdenador }: IOrdenador) {
+  const [aberto, setAberto] = useState(false);
+  const ordenadorNome = ordenador && opcoes.find(opcao => opcao.value === ordenador)?.nome
+
+
+  return (
+    <button className={classNames({
+      [styles.ordenador]: true,
+      [styles["ordenador--ativo"]]: ordenador !== ""
+    })}
+      onClick={() => setAberto(!aberto)}
+      onBlur={() => setAberto(false)}
+    >
+      <span> {ordenadorNome || "Ordenar por:"} </span>
+      {aberto ? <MdKeyboardArrowUp size={20} /> : <MdKeyboardArrowDown size={20} />}
+      <div className={classNames({
+        [styles.ordenador__options]: true,
+        [styles['ordenador__options--ativo']]: aberto
+      })} >
+        {opcoes.map(opcao => (
+          <div className={styles.ordenador__option} key={opcao.value} onClick={() => setOrdenador(opcao.value)}>
+            {opcao.nome}
+          </div>
+        ))}
+      </div>
+    </button>
+  )
+}
